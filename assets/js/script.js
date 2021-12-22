fa2596fc20beca25f14193accc8e56658185ea30
Diego Ávila Maturana <94457305+AvilaMaturana@users.noreply.github.com>
2021-12-22 12:21:19 -0300

    Detalles antes del Contacto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+});
+
 
 $(document).ready(function () {
 
@@ -24,6 +25,5 @@ $("a").on('click', function(event) {
   };
 });
 
+});
 
-
-})
